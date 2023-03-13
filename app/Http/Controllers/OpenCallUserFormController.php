0d367122615064245755e52d4383af72c99ebf05
Tarang Panchal <tarang.p@biztechcs.com>
2023-03-13 11:44:25 +0530

    add : Make Controller Clean

--- app/Http/Controllers/OpenCallUserFormController.php
+++ app/Http/Controllers/OpenCallUserFormController.php
@@ -3,19 +3,16 @@
 namespace App\Http\Controllers;
 
 use App\Models\OpenCall;
-use Illuminate\Support\Str;
-use App\Models\OpenCallResponse;
-use Illuminate\Support\Facades\Mail;
 use App\Http\Requests\CreateOpenCallUserForm;
-use App\Mail\OpenCallFormFilledMail;
-use App\Repositories\UploadFileRepository;
+use App\Services\OpenCallFormService;
 
 class OpenCallUserFormController extends Controller
 {
 
-    public function __construct(public UploadFileRepository $uploadFileRepository) {
-        $this->uploadFileRepository = $uploadFileRepository;
+    public function __construct(private OpenCallFormService $openCallUserFormService) {
+        $this->openCallUserFormService = $openCallUserFormService;
     }
+
     /**
      * Display a form page for opencall.
      *
@@ -39,39 +36,7 @@ class OpenCallUserFormController extends Controller
      */
     public function store(CreateOpenCallUserForm $request)
     {
-        $response = $request->validated();
-
-        $customItems = array_filter($response, function($key) {
-            return strpos($key, 'custom_') === 0;
-        }, ARRAY_FILTER_USE_KEY);
-
-        foreach ($customItems as $key => $val) {
-
-            if (is_object($val)) {
-                $val = $this->uploadFileRepository->uploadFile('front_opencall', $val, $response['open_call_id'].'/'.Str::slug($response['name']));
-            }
-
-            $newData[] = [str_replace("custom_", "", $key) => $val];
-        }
-
-        //Art Work Image Upload
-        if ($request->file('art_work_image')) {
-            foreach ($request->file('art_work_image') as $file) {
-                $artwork[] = $this->uploadFileRepository->uploadFile('front_opencall', $file, $response['open_call_id'].'/'.Str::slug($response['name']));
-            }
-
-            $response['art_work_title'] = json_encode($response['art_work_title']);
-            $response['art_work_size'] = json_encode($response['art_work_size']);
-            $response['art_work_medium'] = json_encode($response['art_work_medium']);
-            $response['art_work_image'] = json_encode($artwork);
-        }
-
-        $response['other_field'] = json_encode($newData);
-        OpenCallResponse::create($response);
-
-        Mail::to(config('mail.from.address'))->send(new OpenCallFormFilledMail('Admin', $response));
-        Mail::to($response['email'])->send(new OpenCallFormFilledMail('User', $response));
-
+        $this->openCallUserFormService->store($request->validated(), $request->file('art_work_image'));
         return redirect(route('opencall.thanks'));
     }
 
