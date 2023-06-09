e3c5089bfd3c48274c5e350cd580e391ef1d5663
wiswebapp <tarang.webinfosolutions@gmail.com>
2023-06-09 23:02:22 +0530

    fix changes as per reference

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -48,18 +48,20 @@
             <div class="row col-12 col-md-10">
                 @forelse($pageData as $item)
                 <div class="panel panel-default col-12 col-md-6 mb-4 card animated fadeIn">
-                    <div class="panel-body">
-                        <div style="background: #f9f9f9; padding:0 30px 30px 30px;height: 300px;" align="center">
-                            <p style="font-size: 15px;">{{$item->item_title}}</p>
-                            <a style="height:100%;" class="card-title shooting-star-gallery" href="{{asset('images/shop_item/'.$item->item_filename)}}"
+                    <div class="panel-body" style="position: relative;">
+                        <div class="shop-item-div">
+                            <a
+                                class="card-title shooting-star-gallery" href="{{asset('images/shop_item/'.$item->item_filename)}}"
                                 title="{{$item->item_title}}" data-lcl-txt="{{$item->item_description}}"
                                 data-lcl-author="{{$item->artist->artist_name}}"
-                                data-lcl-thumb="{{asset('images/shop_item/'.$item->item_filename)}}">
-                                <img style="height:100%;" src="{{asset('images/shop_item/'.$item->item_filename)}}" class="card-img-top" alt="image title">
+                                data-lcl-thumb="{{asset('images/shop_item/'.$item->item_filename)}}"
+                            >
+                                <img src="{{asset('images/shop_item/'.$item->item_filename)}}" class="card-img-top" alt="image title">
                             </a>
                         </div>
                     </div>
                     <div class="panel-footer" style="padding: 0 5px">
+                        <p style="font-size: 15px;font-weight: bold;">{{$item->item_title}}</p>
                         <div class="subtitle mt-2"><p class="shop-image-p" style="overflow-wrap: break-word;">{!! $item->item_description !!}</p></div>
                         <p class="date"><strong>£ {{number_format($item->item_price, 2)}}</strong></p>
                         <p onclick="alert('Coming soon')" style="margin: 0px;cursor: pointer;" class="btn btn-sm btn-default">Buy Now</p>
